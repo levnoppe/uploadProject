87c2d319f05186ffd52889256047f43366caf31a
levnoppe <noppe.lev@gmail.com>
2020-10-03 14:47:28 +0300

    firebase added bottom sheet added multi files upload works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatBottomSheet, MatBottomSheetRef} from '@angular/material/bottom-sheet';
+import { AngularFireStorage, AngularFireStorageReference, AngularFireUploadTask } from 'angularfire2/storage';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: []
 })
-export class AppComponent {
-  title = 'uploadProject';
+export class AppComponent  {
+  title = 'uploadTest';
+
+
+
+  constructor(private _bottomSheet: MatBottomSheet) {
+
+  }
+
+  openBottomSheet(): void {
+    this._bottomSheet.open(UploadBottomSheetComponent);
+  }
+}
+
+@Component({
+  selector: 'app-upload-bottom-sheet',
+  templateUrl: 'uploadBottomSheet.html',
+  providers: []
+})
+export class UploadBottomSheetComponent {
+
+  ref: AngularFireStorageReference;
+  task: AngularFireUploadTask;
+  uploadProgress: Observable<number>;
+  uploadState: Observable<string>;
+
+  uploads: any[];
+  allProgress: Observable<any>;
+
+  constructor(private _bottomSheetRef: MatBottomSheetRef<UploadBottomSheetComponent>,
+              private afStorage: AngularFireStorage) {}
+
+  upload(event) {
+
+    this.uploads = [];
+    const filelist = event.target.files;
+    const allProgress: Observable<number>[] = [];
+
+    for (const file of filelist) {
+      const id = Math.random().toString(36).substring(2);
+      const ref = this.afStorage.ref(id);
+      const task = ref.put(file);
+      const uploadState = task.snapshotChanges().pipe(map(s => s.state));
+      const uploadProgress = task.percentageChanges();
+      allProgress.push(uploadProgress);
+
+      const uploadTrack = {
+        fileName: file.name,
+        uploadProgress,
+        uploadState,
+        task
+      };
+      this.uploads.push(uploadTrack);
+    }
+
+  }
+
+  closeBottomSheet(): void {
+    this._bottomSheetRef.dismiss();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent, UploadBottomSheetComponent} from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadBottomSheetComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatBottomSheetModule,
+    MatExpansionModule,
+    AngularFireModule.initializeApp({
+      apiKey: environment.firebaseAPIKey,
+      authDomain: 'uploadproject-e53b0.firebaseapp.com',
+      databaseURL: 'https://uploadproject-e53b0.firebaseio.com',
+      storageBucket: 'gs://uploadproject-e53b0.appspot.com'
+    }),
+    AngularFireStorageModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
