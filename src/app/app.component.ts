532c646bfea2e76852e711ad5273e2bcddda3e5c
levnoppe <noppe.lev@gmail.com>
2020-10-03 16:21:53 +0300

    file search from upper button most styling added still no scroller no finish icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {MatBottomSheet, MatBottomSheetRef} from '@angular/material/bottom-sheet';
+import {Component, Inject, InjectionToken} from '@angular/core';
+import {MatBottomSheet, MatBottomSheetRef, MAT_BOTTOM_SHEET_DATA} from '@angular/material/bottom-sheet';
 import { AngularFireStorage, AngularFireStorageReference, AngularFireUploadTask } from 'angularfire2/storage';
 import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
@@ -8,8 +8,7 @@ import {map} from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: []
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent  {
   title = 'uploadTest';
@@ -17,18 +16,19 @@ export class AppComponent  {
 
 
   constructor(private _bottomSheet: MatBottomSheet) {
-
   }
 
-  openBottomSheet(): void {
-    this._bottomSheet.open(UploadBottomSheetComponent);
+  openBottomSheet(event): void {
+    this._bottomSheet.open(UploadBottomSheetComponent,{
+      data: { event },
+    });
   }
 }
 
 @Component({
   selector: 'app-upload-bottom-sheet',
   templateUrl: 'uploadBottomSheet.html',
-  providers: []
+  styleUrls: ['uploadBottomSheet.css']
 })
 export class UploadBottomSheetComponent {
 
@@ -36,19 +36,27 @@ export class UploadBottomSheetComponent {
   task: AngularFireUploadTask;
   uploadProgress: Observable<number>;
   uploadState: Observable<string>;
-
+  toggleIcon = 'expand_more';
+  cancelIcon = 'delete';
+  filesNum: string;
   uploads: any[];
   allProgress: Observable<any>;
 
+
   constructor(private _bottomSheetRef: MatBottomSheetRef<UploadBottomSheetComponent>,
-              private afStorage: AngularFireStorage) {}
+              private afStorage: AngularFireStorage,
+              @Inject(MAT_BOTTOM_SHEET_DATA) public data: any) {
+    console.log(data);
+    this.upload(data.event);
+  }
 
   upload(event) {
 
     this.uploads = [];
     const filelist = event.target.files;
+    this.filesNum = filelist.length.toString();
     const allProgress: Observable<number>[] = [];
-
+    let downloadURL: Observable<string>;
     for (const file of filelist) {
       const id = Math.random().toString(36).substring(2);
       const ref = this.afStorage.ref(id);
@@ -56,19 +64,33 @@ export class UploadBottomSheetComponent {
       const uploadState = task.snapshotChanges().pipe(map(s => s.state));
       const uploadProgress = task.percentageChanges();
       allProgress.push(uploadProgress);
+      const fileSize = (file.size.toPrecision(2) / 1000000) + 'M';
+      task.snapshotChanges().pipe(map(s => s.ref.getDownloadURL().then((url) => {downloadURL = url; })))
 
+      ;
       const uploadTrack = {
         fileName: file.name,
+        fileSize,
         uploadProgress,
         uploadState,
-        task
+        task,
+        downloadURL
       };
       this.uploads.push(uploadTrack);
     }
-
   }
 
   closeBottomSheet(): void {
     this._bottomSheetRef.dismiss();
+  }
+
+  toggle(){
+    if (this.toggleIcon === 'expand_more')
+    {
+      this.toggleIcon = 'expand_less';
+    }
+    else {
+      this.toggleIcon = 'expand_more'
+    }
   }
 }
